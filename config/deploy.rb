004ab08022d324fefdacd37446d462d5625aa533
h0-ub4 <hav0k@me.com>
2016-09-07 16:52:55 +0300

    изменено:      app/views/layouts/application.html.haml 	изменено:      config/deploy.rb 	изменено:      config/deploy/production.rb 	новый файл:    db/seeds.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, 'Bender'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@bitbucket.org:alphav/bender.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
