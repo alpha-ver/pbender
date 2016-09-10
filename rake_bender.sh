09605316cd6833643f15f757193768de7b39134a
h0-ub4 <hav0k@me.com>
2016-09-10 17:24:36 +0300

    изменено:      rake_bender.sh

--- rake_bender.sh
+++ rake_bender.sh
@@ -4,17 +4,17 @@ export PATH=$PATH:/usr/local/lib/ruby/2.3.0/rubygems
 case $1 in
    start)
       cd /app/bender/current;
-      echo $$ > /app/bender/current/tmp/pids/metrics_gen_master.pid
-      /app/bender/current/bin/rake bender RAILS_ENV=production >> /app/bender/current/log/rake_bender.log
+      #echo $$ > /app/bender/current/tmp/pids/metrics_gen_master.pid
+      /app/bender/current/bin/rake bender RAILS_ENV=production >> /app/bender/current/log/rake_bender.log &
       ;;
     stop)
       kill `cat /app/bender/current/tmp/pids/metrics_gen.ppid`
       kill `cat /app/bender/current/tmp/pids/metrics_gen.pid`
-      kill `cat /app/bender/current/tmp/pids/metrics_gen_master.pid`
+      #kill `cat /app/bender/current/tmp/pids/metrics_gen_master.pid`
 
       rm -rf /app/bender/current/tmp/pids/metrics_gen.ppid
       rm -rf /app/bender/current/tmp/pids/metrics_gen.pid
-      rm -rf /app/bender/current/tmp/pids/metrics_gen_master.pid
+      #rm -rf /app/bender/current/tmp/pids/metrics_gen_master.pid
 
       ;;
     *)
