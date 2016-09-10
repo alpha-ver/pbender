ccf283f8fba7851d06eb68ccbc4245a69abd36f3
h0-ub4 <hav0k@me.com>
2016-09-10 21:09:39 +0300

    MONIT

--- rake_bender.sh
+++ rake_bender.sh
@@ -5,16 +5,19 @@ case $1 in
    start)
       cd /app/bender/current;
       #echo $$ > /app/bender/current/tmp/pids/metrics_gen_master.pid
-      /app/bender/current/bin/rake bender RAILS_ENV=production >> /app/bender/current/log/rake_bender.log &
+      unbuffer /app/bender/current/bin/rake bender RAILS_ENV=production >> /app/bender/current/log/rake_bender.log &
       ;;
     stop)
-      kill `cat /app/bender/current/tmp/pids/metrics_gen.ppid`
-      kill `cat /app/bender/current/tmp/pids/metrics_gen.pid`
-      #kill `cat /app/bender/current/tmp/pids/metrics_gen_master.pid`
-
+      
+      kill -0 `cat /app/bender/current/tmp/pids/metrics_gen.ppid`
+      wait `cat /app/bender/current/tmp/pids/metrics_gen.ppid`
       rm -rf /app/bender/current/tmp/pids/metrics_gen.ppid
+      echo 'ppid !'
+
+      kill -0 `cat /app/bender/current/tmp/pids/metrics_gen.pid`
+      wait cat /app/bender/current/tmp/pids/metrics_gen.pid
       rm -rf /app/bender/current/tmp/pids/metrics_gen.pid
-      #rm -rf /app/bender/current/tmp/pids/metrics_gen_master.pid
+      echo 'pid!'
 
       ;;
     *)
